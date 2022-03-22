2d8e259f1a57d6d846584677165b37691cf487a9
marocks31 <mollyarisley@gmail.com>
2022-03-21 21:15:32 -0500

    created contacts controller RESTful actions, added in gem for authentication in gemfile, added in RESTful routes, added user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  
+  get "/contacts" => "contacts#index"
+  post "/contacts" => "contacts#create"
+  get "/contacts/:id" => "contacts#show"
+  patch "/contacts/:id" => "contacts#update"
+  delete "/contacts/:id" => "contacts#destroy"
+  
+  
+  post "/users" => "users#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Contact.create(first_name: "Jay", last_name: "Rock", email: "jayrocksss@test.com", phone_number: 8388482013)
+
+Contact.create(first_name: "Charlie", last_name: "Herrera", email: "charming@test.com", phone_number: 3849392394)
+
+
+puts "Done. "
