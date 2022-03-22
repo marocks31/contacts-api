23b79e0664a274c533e8c36c7f75c967004c6e59
marocks31 <mollyarisley@gmail.com>
2022-03-21 22:04:35 -0500

    issues with contacts controller... request to update not functioning

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < ApplicationController
 
 def index
-  contacts = Contacts.all
+  contacts = Contact.all
   render json: contacts.as_json
 end 
 
@@ -9,7 +9,7 @@ end
 def create
   contact = Contact.new(
     first_name: params[:first_name],          
-    last_name: params[:last_name] ,           
+    last_name: params[:last_name],           
     email: params[:email],     
     phone_number: params[:phone_number] 
   )
@@ -18,16 +18,17 @@ def create
 end
 
 def show
-  contact = Contact.find_by(:id params[:id])
+  contact = Contact.find_by(id: params[:id])
   render json: contact.as_json
 end
 
 def update
-  contact = Contact.find_by(:id params[:id])
+  contact = Contact.find_by(id: params[:id])
   contact.first_name = params[:first_name] || contact.first_name
   contact.last_name = params[:last_name] || contact.last_name
   contact.email = params[:email] || contact.email
   contact.phone_number = params[:phone_number] || contact.phone_number
+
   contact.save
   render json: contact.as_json
 end 
